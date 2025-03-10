0ab3497fe569f9e12076806c731506e5e19bf2bc
Facundo Alvarez <turcoalv@gmail.com>
2025-03-10 14:13:26 -0300

    fix format

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -17,7 +17,8 @@ class Order extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function items(): HasMany {
+    public function items(): HasMany
+    {
         return $this->hasMany(OrderItem::class);
     }
 
