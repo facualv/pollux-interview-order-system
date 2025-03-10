6c5cd49baed6f0ad87cede82f8ee651e089b98cf
Facundo Alvarez <turcoalv@gmail.com>
2025-03-10 13:03:13 -0300

    migrations

---
+++ database/migrations/2025_03_10_150340_create_orders_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->foreignId('user_id')->constrained('users');
+            $table->enum('type', ['delivery', 'pickup'])->default('delivery');
+            $table->enum('status', ['received', 'canceled', 'delivered'])->default('received');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
