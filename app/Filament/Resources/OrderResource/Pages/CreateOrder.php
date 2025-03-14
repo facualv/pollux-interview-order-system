66f2a556dcc7be56ccc2d801bee61a3722cfc164
Facundo Alvarez <turcoalv@gmail.com>
2025-03-14 14:02:03 -0300

    fix: remove create another from orders

--- app/Filament/Resources/OrderResource/Pages/CreateOrder.php
+++ app/Filament/Resources/OrderResource/Pages/CreateOrder.php
@@ -3,12 +3,13 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateOrder extends CreateRecord
 {
     protected static string $resource = OrderResource::class;
+
+    protected static bool $canCreateAnother = false;
 
     protected function getRedirectUrl(): string
     {
